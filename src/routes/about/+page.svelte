99da98f1e1068de2003c29b593aa0abe56feb618
dyckdavid <dyckd27@gmail.com>
2023-05-17 15:09:20 -0600

    Initial commit

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Acerca Arco Trailers</h1>
+
+	<p>
+		cooming soon
+	</p>
+
+	
+</div>
