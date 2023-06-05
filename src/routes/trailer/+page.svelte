31503de191d8a8dd68127fc963f6c3c3db711581
dyckdavid <dyckd27@gmail.com>
2023-06-05 17:37:03 -0600

    update

--- src/routes/trailer/+page.svelte
+++ src/routes/trailer/+page.svelte
@@ -18,7 +18,7 @@
 <div class="flex items-center h-screen space-x-4 pl-10">
     <img src={trailer_one} class="w-78 h-78 object-cover -ml-80" alt="Description">
     <div class="text-right flex-grow">
-        <h1 class="text-4xl font-bold mb-2">Remolque 1</h1>
+        <h1 class="text-4xl font-bold mb-2">Remolque de 24'</h1>
         <p>Descripcion del Remolque no. 1</p>
     </div>
 </div>
@@ -27,7 +27,7 @@
 <div class="flex items-center h-screen space-x-4 px-4 md:flex-row flex-col md:px-4 px-2">
     <img src={trailer_one} class="w-78 h-78 object-cover -ml-80 mx-auto md:mx-0" alt="Description">
     <div class="text-center md:text-right flex-grow">
-        <h1 class="text-4xl font-bold mb-2">Remolque 2</h1>
+        <h1 class="text-4xl font-bold mb-2">Remolque de 16'</h1>
         <p>Descripcion del Remolque no. 2</p>
     </div>
 </div>
@@ -36,7 +36,7 @@
 <div class="flex flex-col items-center md:items-start h-screen space-x-0 md:space-x-4 px-4 md:flex-row">
     <img src={trailer_one} class="w-48 h-48 object-cover md:-ml-12 mx-auto" alt="Description">
     <div class="text-center md:text-right flex-grow">
-        <h1 class="text-4xl font-bold mb-2">Remolque 3</h1>
+        <h1 class="text-4xl font-bold mb-2">Remolque 14'</h1>
         <p>Descripcion del Remolque no. 3</p>
     </div>
 </div>
